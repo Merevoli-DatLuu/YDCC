168f2e18e4538e772297503c897540ceb790dc01
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2021-08-23 21:52:05 +0700

    [Add]: setting.py

--- back-end/YDCC/YDCC/settings.py
+++ back-end/YDCC/YDCC/settings.py
@@ -29,6 +29,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 # Application definition
 
@@ -46,6 +50,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'dj_rest_auth.registration',
     'allauth.socialaccount',
+    'corsheaders',
     'user.apps.UserConfig',
     'citizen.apps.CitizenConfig',
 
@@ -54,6 +59,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
